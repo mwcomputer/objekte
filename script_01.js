75a99225edb8834a1b72e7b18e2eee3fae1c8ef4
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-05-25 19:41:15 +0200

    Add files via upload

---
+++ script_01.js
@@ -0,0 +1,13 @@
+
+
+/***** Erinnerung: Arrays *******/
+// let arr;
+// arr = ["Ich","bin","Max"];
+// ausgabe(arr[2]);
+
+
+
+/** Ausgabe */
+function ausgabe(outputStr) {
+        console.log(outputStr);
+    }
