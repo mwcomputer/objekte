139ca0dbd4fb7b750fc5c26b2228d1fcaac26a7e
mwcomputer <kolbenfresser5@googlemail.com>
2021-05-26 14:43:56 +0200

    hallo

--- script_01.js
+++ script_01.js
@@ -1,9 +1,65 @@
 
 
 /***** Erinnerung: Arrays *******/
-// let arr;
-// arr = ["Ich","bin","Max"];
-// ausgabe(arr[2]);
+ let arr;
+ arr = ["Ich","bin","Max"];
+ //ausgabe(arr[2]);
+
+ //arr =[ ["Ich","bin","Max"],  ["Ich","bin","Moritz"]];
+ //ausgabe(arr[0][2]);
+ //ausgabe(arr[1][2]);
+
+
+/***** Objekte 1 Daten/Funktionen *******/
+
+let person = { 
+    firstName:"Deniz" ,
+               familyName:"Yildiz" ,
+               salary:[120000, 160000], 
+               hasAccess: true,
+               sayHello: 
+               function()
+               {
+                   return "Hi ich bin " + this.firstName;
+               }
+};
+             
+//ausgabe(person);
+//ausgabe(person.firstName);
+//ausgabe(person.familyName);
+//ausgabe(person.salary[0]);
+//ausgabe(person.hasAccess);
+//ausgabe(person.sayHello());
+
+let txt = "ich bin " + person.firstName + " " +
+                       person.familyName + " und verdiene " +
+                       person.salary[1] + "p.a.";
+
+//ausgabe(txt);      
+/*////Objekte 2 Herachie /********/
+
+// Tiefer, tiefer - Irgendwo in der Tiefe gibt es ein Licht // K.Bush
+
+// Der Baikalsee ist mit 1642 m der tiefste See der Erde. ...
+
+// deep - deeper - deepest 
+
+// Nested Object 
+
+let baikal ={
+            wert: "10m" ,
+            deep: {
+                   deeper:{
+                       deepest: "das Licht - auf 1642 m"
+                   }
+            }
+}
+
+
+
+ausgabe(baikal);
+ausgabe(baikal.wert);
+ausgabe(baikal.deep.deeper.deepest);
 
 
 
@@ -11,3 +67,5 @@
 function ausgabe(outputStr) {
         console.log(outputStr);
     }
+
+
